4ec8835618cdd016f4d947fdd9d7d73100267e03
Lars Boegild Thomsen <lth@netcompartner.com>
2024-11-13 09:17:06 +0800

    Moved FreeRTOS heap to CCMRAM and enabled watchdog

--- stm32world_freertos2/Core/Inc/FreeRTOSConfig.h
+++ stm32world_freertos2/Core/Inc/FreeRTOSConfig.h
@@ -72,7 +72,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)10000)
+#define configTOTAL_HEAP_SIZE                    ((size_t)65536)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configGENERATE_RUN_TIME_STATS            1
 #define configUSE_TRACE_FACILITY                 1
@@ -180,6 +180,7 @@ standard names. */
 
 /* USER CODE BEGIN Defines */
 /* Section where parameter definitions can be added (for instance, to override default ones in FreeRTOS.h) */
+#define configAPPLICATION_ALLOCATED_HEAP 1
 /* USER CODE END Defines */
 
 #endif /* FREERTOS_CONFIG_H */
